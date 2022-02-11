1804d5f59f99638052ecf564f600be2b8485ee7e
manishourya <73073043+manishourya@users.noreply.github.com>
2022-02-11 13:30:48 +0530

    Add files via upload

--- MainController.java
+++ MainController.java
@@ -1,41 +1,30 @@
-package com.ecommerce.controller;
+package com.ecommerce.controllers;
 
-import org.apache.log4j.Logger;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-
+import com.ecommerce.beans.Quote;
 
 @Controller
 public class MainController {
 
-
-        
-           @RequestMapping(value = "/customevent", method = RequestMethod.GET)
-            public String customEvent(ModelMap map)
-            {
-                    String confFile = "main-servlet.xml";
-                ApplicationContext context = new ClassPathXmlApplicationContext(confFile);
-                CustomEventPublisher cvp =
-                        (CustomEventPublisher) context.getBean("customEventPublisher");
-                     
-                 cvp.publish();  
-                 cvp.publish();
-                return "customEvent";
-            }
+         @RequestMapping("/")
+          @ResponseBody
+          public String index() {
+                 
+                 RestTemplate restTemplate = new RestTemplate();
+             Quote quote = restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+                
+            return quote.toString();
+          }
            
+        
 }
-
-
 
---
+++ SpringRestClienApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestClienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestClienApplication.class, args);
+	}
+
+}
