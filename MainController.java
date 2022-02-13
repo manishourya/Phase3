3ea22e50d83b785385d3a2b3502630eb2c4f30d4
manishourya <73073043+manishourya@users.noreply.github.com>
2022-02-13 10:52:06 +0530

    Add files via upload

--- MainController.java
+++ MainController.java
@@ -1,5 +1,6 @@
 package com.ecommerce.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -8,23 +9,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
 
-import com.ecommerce.beans.Quote;
+
+
 
 @Controller
 public class MainController {
 
+         @Autowired
+         private ProductRepository repository;
+         
          @RequestMapping("/")
           @ResponseBody
           public String index() {
-                 
-                 RestTemplate restTemplate = new RestTemplate();
-             Quote quote = restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-                
-            return quote.toString();
+               
+
+            return “This is running under SSL”;
           }
-           
+         
         
 }
-
